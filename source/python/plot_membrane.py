0c0cad8ac988ac8cbf207d6db94960732bed89d6
Andreas Hartel <ahartel@belinea.(none)>
2013-01-06 14:29:40 +0100

    simplified and improved plotting scripts

--- source/python/plot_membrane.py
+++ source/python/plot_membrane.py
@@ -1,12 +1,10 @@
 import sys 
 import matplotlib.pyplot as plt
 
-#plt.figure()
-#plt.subplot(121)
-plt.plotfile(sys.argv[1],(0,1))#,newfig=False)
-plt.title('neuroCL')
-#plt.subplot(122)
-plt.plotfile(sys.argv[2],(0,1))
-plt.title('compare')
+
+for argument in range(1,len(sys.argv),2):
+	plt.plotfile(sys.argv[argument],(0,int(sys.argv[argument+1])))
+
 plt.show()
+
 
--- source/python/plot_spikes.py
+++ source/python/plot_spikes.py
@@ -1,12 +1,9 @@
 import sys 
 import matplotlib.pyplot as plt
 
-#plt.figure()
-#plt.subplot(121)
-plt.plotfile(sys.argv[1],(0,1),linestyle='',marker='|')#,newfig=False)
-plt.title('neuroCL')
-#plt.subplot(122)
-plt.plotfile(sys.argv[2],(0,1),linestyle='',marker='|')
-plt.title('compare')
+for argument in range(1,len(sys.argv),1):
+	plt.plotfile(sys.argv[argument],(0,1),linestyle='',marker='|')
+
 plt.show()
+
 
