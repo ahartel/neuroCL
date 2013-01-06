2b85d26e3d8d9c1a96abb83e095d4868a04cef5d
Andreas Hartel <ahartel@belinea.(none)>
2013-01-06 14:31:38 +0100

    added network functionality to compare program, broke compatibility to openCL version

--- source/compare/fire_evolve.cpp
+++ source/compare/fire_evolve.cpp
@@ -9,17 +9,25 @@ using namespace std;
 int main()
 {
 	INIT_TIMER(complete)
-
+	// neuron variables
 	float membranes[N];
 	float u[N];
 	float d[N];
 	float a[N];
-	float I[N];
+	// input current
+	float I[N][D];
+	int delay_pointer = 0;
+	// network parameters
+	float weights[N][M];
+	int delays[N][M];
+	int post_neurons[N][M];
+	int num_post[N];
+	// spike storage
 	unsigned int spikes[int(N*T*h/10.)];
 	unsigned int k[T];
 	for (unsigned int i=0; i<T; i++) k[i] = 0;
 
-	init_neurons(membranes,u,d,a,I);
+	init_neurons(membranes,u,d,a,I,weights,delays,post_neurons,num_post);
 
 	unsigned int total_spikes = 0;
 
@@ -33,6 +41,9 @@ int main()
 	INIT_TIMER(loops)
 	for (unsigned int t=0; t<T; t++)
 	{
+		// random thalamic input for 1 in 1000 neurons
+		for (int j=0;j<N/1000;j++)
+			I[getrandom(N)][delay_pointer] += 20.0;
 		// reset loop
 		for (int i=0; i<N; i++)
 		{
@@ -43,15 +54,42 @@ int main()
 				spikes[total_spikes++] = i;
 			}
 		}
+
+		// transmit loop
+		int last_spike_count;
+		if (t > 0)
+		{
+ 			last_spike_count = k[t-1];
+		}
+		else
+			last_spike_count = 0;
+
+		for (int i=last_spike_count; i<total_spikes; i++)
+		{
+			int neuronID = spikes[i];
+			for (int m=0; m<num_post[neuronID]; m++)
+			{	
+				I[post_neurons[neuronID][m]][(delays[neuronID][m]+delay_pointer+1)%D] += weights[neuronID][m];	
+				//if (post_neurons[neuronID][m]==0)
+				//{
+				//	cout << "Time " << t << ": Injecting current to neuron " << post_neurons[neuronID][m] << " from neuron " << neuronID << " into delay slot " << (delays[neuronID][m]+delay_pointer+1)%D << endl;
+				//	cout << I[post_neurons[neuronID][m]][(delays[neuronID][m]+delay_pointer+1)%D] << endl;
+				//}
+			}
+		}
+
 		// evolve loop
 		for (int i=0; i<N; i++)
 		{
-			membranes[i] += h * 0.5 * (( 0.04 * membranes[i] * membranes[i]) + (5.0 * membranes[i] + (140.0 - u[i] + I[i] )));
-			membranes[i] += h * 0.5 * (( 0.04 * membranes[i] * membranes[i]) + (5.0 * membranes[i] + (140.0 - u[i] + I[i] )));
+
+			membranes[i] += h * 0.5 * (( 0.04 * membranes[i] * membranes[i]) + (5.0 * membranes[i] + (140.0 - u[i] + I[i][delay_pointer] )));
+			membranes[i] += h * 0.5 * (( 0.04 * membranes[i] * membranes[i]) + (5.0 * membranes[i] + (140.0 - u[i] + I[i][delay_pointer] )));
 
 			u[i] += h * a[i]*(0.2*membranes[i]-u[i]);
-		}
 
+			//reset current
+			I[i][delay_pointer] = 0;
+		}
 #ifdef WATCH_NEURONS
 		for (unsigned int i=0; i < num_watched_neurons; i++)
 		{
@@ -62,12 +100,18 @@ int main()
 		}
 #endif
 		k[t] = total_spikes;
-		//cout << "Num spikes after " << t << " ms: " << k[t] << endl;
+		cout << "Num spikes after " << t << " ms: " << k[t] << endl;
+
+		// jump to next delay entry
+		delay_pointer++;
+		if (delay_pointer == D)
+			delay_pointer = 0;
 	}
 
 	STOP_TIMER("loops",loops)
 
 	cout << "found: " << k[T-1] << endl;
+
 
 	ofstream myfile;
 #ifdef WATCH_NEURONS
--- source/helpers.h
+++ source/helpers.h
@@ -1,28 +1,37 @@
 #include <stdlib.h>
+#include <iostream>
 /*
  * Constants
  */
-const int Ne = 80;//12800;
-const int Ni = 20;//3200;
+const int Ne = 800;//12800;
+const int Ni = 200;//3200;
 const int N = Ne+Ni; // total number of neurons
-const int M = 1; // number of postsynaptic neurons
+const static int M = 100; // number of postsynaptic neurons
 const float v_thresh = 30./*mV*/;
 const float v_reset = -65/*mV*/;
-const int T = 1000; // number of timesteps
+const int T = 10000; // number of timesteps
 const float h = 1; // timestep in milliseconds
+const static int D = 20; // max. delay in ms
+
+using namespace std;
+
+#define getrandom(max1) ((rand()%(int)((max1)))) // random integer between 0 and max-1
 
 #define TIMING
-//#define WATCH_NEURONS
-//#define WATCH_ADAPTATION
+#define WATCH_NEURONS
+#undef WATCH_ADAPTATION
 
-void init_neurons(float* membranes, float* u, float* d, float* a, float* I)
+void init_neurons(float* membranes, float* u, float* d, float* a, float I[N][D], float weights[N][M], int delays[N][M], int post_neurons[N][M], int* num_post)
 {
-	srand(23);
+	srand(42);
 	for (int i=0; i<N; i++) {
 		//membranes[i] = (float)rand()/float(RAND_MAX)*50.0;
 		membranes[i] = v_reset;
-		//I[i] = 10.0;
-		I[i] = (float)rand()/float(RAND_MAX)*10.0;
+
+		for (int d=0; d<D; d++)
+		{
+			I[i][d] = 0;//(float)rand()/float(RAND_MAX)*10.0;
+		}
 		u[i] = 0.2*membranes[i];
 		if (i < Ne) {
 			a[i] = 0.02;
@@ -33,6 +42,19 @@ void init_neurons(float* membranes, float* u, float* d, float* a, float* I)
 			d[i] = 2.0;
 		}
 
+		num_post[i] = getrandom(100);
+
+		for (int m=0; m<num_post[i]; m++)
+		{
+			if (i<Ne)
+				weights[i][m] = 6.;
+			else
+				weights[i][m] = -5.;
+			delays[i][m] = 1;
+			post_neurons[i][m] = getrandom(N);
+			while (post_neurons[i][m] == i)
+				post_neurons[i][m] = getrandom(N);
+		}
 		/*
 		exc_input[i] = 0.0;
 
